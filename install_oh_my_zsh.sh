b7ee855354a3a0129c4f480b93e6790a99647dbc
sittingfrog <jvj.norton@gmail.com>
2022-01-19 00:11:39 -0500

    first commit

---
+++ install_oh_my_zsh.sh
@@ -0,0 +1,6 @@
+
+#https://divinenanny.nl/blog/2021-08-07-install-oh-my-zsh-on-raspberry-pi/
+sudo apt-get install zsh
+chsh -s /bin/zsh
+q
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
