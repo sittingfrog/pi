5d1c853712562581bca6793c21c778b7a38c8fc7
jn <jn@jns-macbook-pro.lan>
2022-02-19 19:34:05 -0800

    l

--- src/install_oh_my_zsh.sh
+++ src/install_oh_my_zsh.sh
@@ -3,5 +3,4 @@
 #https://divinenanny.nl/blog/2021-08-07-install-oh-my-zsh-on-raspberry-pi/
 sudo apt install zsh -y
 sudo chsh -s /bin/zsh -y
-q
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
